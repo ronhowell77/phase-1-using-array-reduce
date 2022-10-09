6c592bbf0679bde576dce41f4172559efd40e366
Ronnie <ron.howell0326@gmail.com>
2022-10-09 10:52:52 -0400

    commit tree 71df5b1a73cdfa4910a2cec0915b9e73dd6942b6

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const reducer = (accumlator, item) => {
+    let total = 0
+    const batteries = () => {
+        let totalBatteries = 0
+        totalBatteries = item
+        return total += totalBatteries
+    }
+        return accumlator += total
+}
+let totalBatteries = batteryBatches.reduce(reducer, 31)
